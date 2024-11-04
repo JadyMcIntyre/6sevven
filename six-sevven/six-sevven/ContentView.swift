b8cc5dc995cd6e5db4f645a997ca07b9dc889f07
JadyMcIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-11-04 10:48:54 +0200

    initial commit

---
+++ six-sevven/six-sevven/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  six-sevven
+//
+//  Created by Jady McIntyre on 2024/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ six-sevven/six-sevven/six_sevvenApp.swift
@@ -0,0 +1,17 @@
+//
+//  six_sevvenApp.swift
+//  six-sevven
+//
+//  Created by Jady McIntyre on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct six_sevvenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
